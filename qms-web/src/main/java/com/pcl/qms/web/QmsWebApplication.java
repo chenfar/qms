90442af959e948d3826fa504b2e31042bbf1e385
chenfar <825655513@qq.com>
2022-04-01 16:15:25 +0800

    init project

---
+++ qms-web/src/main/java/com/pcl/qms/web/QmsWebApplication.java
@@ -0,0 +1,15 @@
+package com.pcl.qms.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author chenFr
+ * @data 2022年04月01日 16:08
+ */
+@SpringBootApplication
+public class QmsWebApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(QmsWebApplication.class);
+    }
+}
